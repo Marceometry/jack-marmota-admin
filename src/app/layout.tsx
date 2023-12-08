68f063cc14a673c333b71d0f06e0333637712f87
Marcelino Teixeira <marcelino.teixeira.dev@gmail.com>
2023-12-08 18:00:24 -0300

    feat: init project

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from 'next'
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'Jack Marmota Admin',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  )
+}
