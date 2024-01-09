e846710446704c1be99940891a393a59477661ab
Marcelino Teixeira <marcelino.teixeira.dev@gmail.com>
2024-01-08 23:13:49 -0300

    fix: typescript errors

--- src/contexts/SetListsContext/utils.ts
+++
@@ -1,8 +0,0 @@
-import { SetListFilters } from '@/contexts'
-import { SetList } from '@/types'
-
-export const setlistFilter = (setlist: SetList, filters: SetListFilters) => {
-  const {} = filters
-
-  return true
-}
